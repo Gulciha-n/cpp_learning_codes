14b3b0f30b74e695dbc315a03ecdbf808fb7a1de
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2023-10-22 08:42:54 +0300

    Add files via upload

---
+++ 01-Intro-1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main(){      //int main : Programın başlangıcı , komutlar burada yer alır
+    cout << "Welcome to C++" << endl;   //  endl = \n
+    cout << "Hi There" << endl << endl << "Hello";
+    cout << endl;
+    return 0;  // Program başarıyla tamamlandığını belirtir
+}
