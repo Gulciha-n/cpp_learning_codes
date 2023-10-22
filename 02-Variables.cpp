6233310dbcd1597c13899732589f82f0af8b15ac
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2023-10-22 08:43:08 +0300

    Add files via upload

---
+++ 02-Variables.cpp
@@ -0,0 +1,20 @@
+#include <iostream> 
+
+using namespace std;
+int main(){
+    string name = "Derya";
+    int age = 20;
+    age = 25;
+    name =  "Pelin";
+    char avr = 'A';  // ASCII'de her char değerinin bir karşılığı var => A = 65  A yerine 65 de yazabiliriz.
+    float numerical_avr = 3.25;
+    bool take_exam = true;  
+
+    cout << "Name : " << name << endl << "Age : " << age << endl;
+    cout << "Average of grades : " << avr << endl;
+    cout << "Numerical Average of grades :" << numerical_avr << endl;
+    cout << "Did the student take the exam? :" << take_exam << endl;
+} 
+
+
+
