717a645b2b3458ef1c1945266754326d6ea54c4a
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2023-10-22 08:45:19 +0300

    Libraries

---
+++ 05-Include_Libraries.cpp
@@ -0,0 +1,25 @@
+#include <iostream> 
+#include <cmath>
+
+using namespace std;
+int main (){
+
+    int y ;
+    float x;
+    float z;
+    float t;
+
+    y = pow(2,5);
+    x = floor(2.5);
+    z = ceil(2.5);
+    t = round(2.6);
+
+    cout << y << endl;
+    cout << x << endl;
+    cout << z << endl;
+    cout << t << endl;
+ 
+    return 0;
+}
+
+
