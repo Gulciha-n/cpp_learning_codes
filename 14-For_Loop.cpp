d2d2ccccc4f862117a2872bc9fd6d2e654f5e960
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2023-10-24 23:01:42 +0300

    For_Loop

---
+++ 14-For_Loop.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    //for
+    for (int i=1 ; i<=20 ; i++){
+        cout << i << endl;
+    }
+
+    for (int a=1 ; a<=20 ; a++){
+        if(a % 2 == 0)
+            cout << a << endl;
+    }
+    
+    //while
+    int j = 1;
+    while(j<=20){
+        cout << j << endl;
+        j++;
+    }
+} 
