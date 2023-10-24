ebcef1ff134a21d9a8e216944d1a6e230952f08e
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2023-10-24 22:59:37 +0300

    While

---
+++ 11-While_Loop.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std;
+
+int main (){
+    int index = 0;
+    while(index<4){
+        cout << "less than 4 " << endl;
+        index++;
+    };   //süslü parantezin sonuna noktalı virgül,while döngüsünün sonlandırılmasını açık bir şekilde ifade eder. koysak da olur koymasak da 
+    return 0;
+}
+
